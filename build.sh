70a669480727ebcb337d1a56621bf264d8f4eefe
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2024-10-24 19:00:46 +0200

    attempt at login

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 source .env
+# source .secrets
+
+use_downloaded=false
+keep=false
+
+# Getting Arguments
+while getopts ku flag; do
+	case "${flag}" in
+	k) keep=true ;;
+	u) use_downloaded=true ;;
+	esac
+done
 
 # Preparing the directories
 if [ ! -d "./bin" ]; then
@@ -19,27 +31,63 @@ if [ ! -d "./config" ]; then
 	mkdir config
 fi
 
-# Getting the latest version of Factorio
-if [ -z "$FACTORIO_VERSION" ]; then
-	echo -e $GREEN"Getting the latest version of Factorio ..."$NC
-	factorio_version=$(curl -s https://factorio.com/get-download/latest/headless/linux64 | grep -oP '(\d+\.\d+\.\d+)' | head -1)
-	echo -e $YELLOW"\tUsing version ${factorio_version} of Factorio"$NC
+if $use_downloaded; then
+	echo -e $GREEN"Using the previous downloaded files!"$NC
 else
-	echo -e $GREEN"Using version ${FACTORIO_VERSION} of Factorio defined in .env file ..."$NC
-	factorio_version=$FACTORIO_VERSION
+
+	# Getting the latest version of Factorio
+	echo -e $GREEN"Getting the latest version of Factorio ..."$NC
+	# https://factorio.com/api/latest-releases
+	factorio_version=$(curl -s https://factorio.com/api/latest-releases | grep -oP '(\d+\.\d+\.\d+)' | head -1)
+	if [ -z "$FACTORIO_VERSION" ]; then
+		echo -e $YELLOW"\tUsing version ${factorio_version} of Factorio"$NC
+	else
+		echo -e $YELLOW"\tLatest version of Factorio: ${factorio_version}"$NC
+		factorio_version=$FACTORIO_VERSION
+		echo -e $GREEN"Using version ${factorio_version} of Factorio defined in .env file ..."$NC
+
+	fi
+
+	# Loging in to Factorio
+	echo -e $GREEN"Logging in to Factorio ..."$NC
+	session=$(curl -s -c - https://www.factorio.com/login | grep -oP 'session\s+\K\S+')
+
+	if [ -z "$FACTORIO_USERNAME" ]; then
+		echo -e $YELLOW"\tUsername: "$NC
+		read username
+	else
+		echo -e $YELLOW"\tUsername: ${FACTORIO_USERNAME}"$NC
+		username=$FACTORIO_USERNAME
+	fi
+
+	if [ -z "$FACTORIO_PASSWORD" ]; then
+		echo -e $YELLOW"\tPassword: "$NC
+		read -s password
+	else
+		echo -e $YELLOW"\tPassword: ********"$NC
+		password=$FACTORIO_PASSWORD
+	fi
+	data="csrf_token=${session}&username_or_email=${username}&password=${password}"
+	# echo -e $data
+	response=$(curl -X POST -d "${data}" -s https://www.factorio.com/login)
+	# not working !!!
+	# TODO: check the response
+	# exit 0
+
+	# Download Factorio headless server
+	if [ -f "./bin/factorio_headless.tar.xz" ]; then
+		echo -e $GREEN"File already exists: ./bin/factorio_headless.tar.xz!\r\n\t"$YELLOW"deleting ./bin/factorio_headless.tar.xz ..."$NC
+		rm -f "./bin/factorio_headless.tar.xz"
+	fi
+
+	echo -e $GREEN"Downloading ...\r\n\t"$YELLOW"https://factorio.com/get-download/${factorio_version}/headless/linux64"$NC
+
+	curl "https://www.factorio.com/get-download/${factorio_version}/headless/linux64?username=${username}&token=${session}" -L --progress-bar --output "./bin/factorio_headless.tar.xz"
+
+	# curl -L --progress-bar "https://factorio.com/get-download/${factorio_version}/headless/linux64" -o "./bin/factorio_headless.tar.xz"
+
 fi
-
-# Download Factorio headless server
-if [ -f "./bin/factorio_headless.tar.xz" ]; then
-	echo -e $GREEN"File already exists: ./bin/factorio_headless.tar.xz!\r\n\t"$YELLOW"deleting ./bin/factorio_headless.tar.xz ..."$NC
-	rm -f "./bin/factorio_headless.tar.xz"
-fi
-
-echo -e $GREEN"Downloading ...\r\n\t"$YELLOW"https://factorio.com/get-download/${factorio_version}/headless/linux64"$NC
-
-#exit 0
-
-curl -L --progress-bar "https://factorio.com/get-download/${factorio_version}/headless/linux64" -o "./bin/factorio_headless.tar.xz"
+# exit 0
 
 # Extracting Factorio headless server
 if [ -d "./bin/factorio" ]; then
@@ -49,3 +97,15 @@ fi
 
 echo -e $GREEN"Extracting ...\r\n\t"$YELLOW"./bin/factorio_headless.tar.xz"$NC
 tar -xf "./bin/factorio_headless.tar.xz" -C "./bin"
+
+# Clean up
+if $keep; then
+	echo -e $GREEN"Keeping the files!"$NC
+	exit 0
+fi
+
+echo -e $GREEN"Cleaning up ...\r\n\t"$YELLOW"deleting ./bin/factorio_headless.tar.xz ..."$NC
+rm -f "./bin/factorio_headless.tar.xz"
+echo -e $YELLOW"\tdeleting ./bin/factirio ..."$NC
+rm -rdf "./bin/factorio"
+echo -e $GREEN"Done!"$NC
